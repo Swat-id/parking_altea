7e68e20d67783647feac23eeee7910652233b64d
SWATID <francisco.sanchis@swat-id.com>
2025-06-26 22:59:53 +0200

    fix: corregir import de config en script de corrección

--- fix_parking_status.py
+++ fix_parking_status.py
@@ -10,7 +10,7 @@ sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-import config
+from src import config
 from models import Base, Parking
 from datetime import datetime
 
