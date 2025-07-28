31a17425e73fbe62acb7226300c7ecd23692318a
SWATID <francisco.sanchis@swat-id.com>
2025-07-28 21:03:20 +0200

    Fix Gear icon import in Alarms.jsx - replace with Settings from lucide-react

--- client/src/pages/Alarms.jsx
+++ client/src/pages/Alarms.jsx
@@ -5,7 +5,7 @@ import {
   AlertCircle, 
   AlertOctagon,
   Bell,
-  Gear,
+  Settings,
   Trash,
   Pencil,
   Eye,
@@ -213,7 +213,7 @@ const Alarms = () => {
                     : 'border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300'
                 }`}
               >
-                <Gear className="h-4 w-4 inline mr-2" />
+                <Settings className="h-4 w-4 inline mr-2" />
                 Configuraciones
               </button>
               <button
@@ -258,7 +258,7 @@ const Alarms = () => {
 
                 {configurations.length === 0 ? (
                   <div className="text-center py-12">
-                    <Gear className="mx-auto h-12 w-12 text-gray-400" />
+                    <Settings className="mx-auto h-12 w-12 text-gray-400" />
                     <p className="mt-4 text-gray-600">No hay configuraciones de alarmas</p>
                   </div>
                 ) : (
